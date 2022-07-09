ddf6f236f129b20af8178b7cfb114e3d1f090757
yshumnyi <shumn00.yura@gmail.com>
2022-07-09 13:11:36 +0300

    ChatBot stage 1

--- chatbot/src/main/kotlin/nix/summer/practice/chatbot/main.kt
+++ chatbot/src/main/kotlin/nix/summer/practice/chatbot/main.kt
@@ -1,6 +1,11 @@
 package nix.summer.practice.chatbot
 
 fun main() {
-    println("Hello! My name is {bot_Dub}. I was created in {2022}")
+    greetings()
+}
 
+fun greetings () {
+    val bot_name = "Ken"
+    val year_of_creating = "2022"
+    println("Hello! My name is $bot_name. I was created in $year_of_creating")
 }
