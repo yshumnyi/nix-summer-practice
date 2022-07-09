18cf90f0a6ead5f023c06a7d5549c603eb61b2c6
yshumnyi <shumn00.yura@gmail.com>
2022-07-09 14:45:06 +0300

    ChatBot stage 3

--- chatbot/src/main/kotlin/nix/summer/practice/chatbot/main.kt
+++ chatbot/src/main/kotlin/nix/summer/practice/chatbot/main.kt
@@ -1,8 +1,12 @@
 package nix.summer.practice.chatbot
 
+import java.lang.Math
+
+
 fun main() {
     greetings_bot()
     greetings_user()
+    guess_age()
 }
 
 fun greetings_bot () {
@@ -14,4 +18,17 @@ fun greetings_bot () {
 fun greetings_user(){
     print("Please, remind me your name.\n What a great name you have, ")
     val user_name= readLine()
+    println("What a great name you have, $user_name!")
+}
+
+fun guess_age() {
+//    var age: Int
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+    val a = readLine()?.toInt()!!
+    val b = readLine()?.toInt()!!
+    val c = readLine()?.toInt()!!
+
+    val age = ( a * 70 + b * 21 + c * 15) % 105
+    println("Your age is $age; that's a good time to start programming!")
 }
