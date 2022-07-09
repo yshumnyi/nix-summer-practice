bb79b44c1a0261b4beaf2ce2735e33419e5a638f
yshumnyi <shumn00.yura@gmail.com>
2022-07-09 13:31:43 +0300

    ChatBot stage 2

--- chatbot/src/main/kotlin/nix/summer/practice/chatbot/main.kt
+++ chatbot/src/main/kotlin/nix/summer/practice/chatbot/main.kt
@@ -1,11 +1,17 @@
 package nix.summer.practice.chatbot
 
 fun main() {
-    greetings()
+    greetings_bot()
+    greetings_user()
 }
 
-fun greetings () {
+fun greetings_bot () {
     val bot_name = "Ken"
     val year_of_creating = "2022"
     println("Hello! My name is $bot_name. I was created in $year_of_creating")
+}
+
+fun greetings_user(){
+    print("Please, remind me your name.\n What a great name you have, ")
+    val user_name= readLine()
 }
