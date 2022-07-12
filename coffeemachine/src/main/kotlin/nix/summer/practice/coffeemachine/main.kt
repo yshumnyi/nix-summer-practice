60d1d50079cfeeabc5d011e0a199644f5b0f17b0
yshumnyi <shumn00.yura@gmail.com>
2022-07-12 11:23:06 +0300

    CoffeeMachine stage 2

--- coffeemachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
+++ coffeemachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
@@ -2,6 +2,7 @@ package nix.summer.practice.coffeemachine
 
 fun main() {
     mass ()
+    calculationIngredients ()
 }
 
 fun mass () {
@@ -12,4 +13,16 @@ fun mass () {
     println("Pouring coffee into the cup")
     println("Pouring some milk into the cup")
     println("Coffee is ready! Go to work!")
+}
+
+fun calculationIngredients () {
+    println("Write how many cups of coffee you will need:")
+    val cupCoffee = readLine()?.toInt()!!
+    var water:Int = 200 * cupCoffee
+    var milk:Int = 50 * cupCoffee
+    var coffeeBeans:Int = 15 * cupCoffee
+    println("For $cupCoffee cups of coffee you will need:")
+    println("$water ml of water")
+    println("$milk ml of milk")
+    println("$coffeeBeans g of coffee beans")
 }
